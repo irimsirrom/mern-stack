d3db3b3405b01f9237091a603304e12e5ea3ea42
Miri Stoner <irimsirrom@gmail.com>
2017-06-20 16:52:07 +0300

    Finished to set up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,12 @@ module.exports = {
   module:{
     loaders: [
       {
-
-      }
-      test: /\.js?$/,
-      exclude: /(node_modules)/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015']
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
     ]
   }
